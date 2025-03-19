b27ed4a719f7c9f541c3ba1ebc98736ba19c4045
Michael Murray <mam95@email.uscb.edu>
2025-03-19 12:25:11 -0400

    inital commit

---
+++ src/main/java/edu/uscb/csci470sp25_team3/echodeck_backend/EchodeckBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25_team3.echodeck_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchodeckBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchodeckBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uscb/csci470sp25_team3/echodeck_backend/EchodeckBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25_team3.echodeck_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchodeckBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
