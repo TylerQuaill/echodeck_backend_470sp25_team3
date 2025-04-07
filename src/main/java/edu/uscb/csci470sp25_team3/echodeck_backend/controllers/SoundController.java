493b0a6845539171bdbe6c2ad87bf25caf780c50
rdsims <rdsims@email.uscb.edu>
2025-04-07 17:33:50 -0400

    Commiting my branch for connecting the backend. Gonna need to be tested before merging to development

--- src/main/java/edu/uscb/csci470sp25_team3/echodeck_backend/controllers/SoundController.java
+++ src/main/java/edu/uscb/csci470sp25_team3/echodeck_backend/controllers/SoundController.java
@@ -2,22 +2,25 @@ package edu.uscb.csci470sp25_team3.echodeck_backend.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.uscb.csci470sp25_team3.echodeck_backend.entities.Sound;
-import edu.uscb.csci470sp25_team3.echodeck_backend.services.SoundService;
+import edu.uscb.csci470sp25_team3.echodeck_backend.repository.SoundRepository;
 
 @RestController
-@RequestMapping("/api/sounds")
+@CrossOrigin(origins = "http://localhost:5173") // Allow requests from your frontend
 public class SoundController {
-    @Autowired
-    private SoundService soundService;
-    @GetMapping
-    public ResponseEntity<List<Sound>> getSounds() {
-        return ResponseEntity.ok(soundService.getAllSounds());
+
+    private final SoundRepository soundRepository;
+
+    public SoundController(SoundRepository soundRepository) {
+        this.soundRepository = soundRepository;
+    }
+
+    @GetMapping("/api/sounds")
+    public List<Sound> getSounds() {
+        return soundRepository.findAll(); // Fetch all sounds from the database
     }
 }
